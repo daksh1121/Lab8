da261c98e870f748dd5e8b2d847739a4141c5dcf
daksh <daksh1ghetia@gmail.com>
2024-03-06 20:11:37 -0500

    Step 5 - Modified methods and added delegate, issue with the last one so modified

--- src/main/java/org/example/myStack.java
+++ src/main/java/org/example/myStack.java
@@ -3,12 +3,12 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class myStack<E> extends ArrayList<E> {
+public class myStack<E> {
 
     private final ArrayList<E> delegate;
 
     public myStack() {
-        delegate = this;
+        delegate = new ArrayList<>();
     }
 
     public void push(E e){
@@ -18,7 +18,7 @@ public class myStack<E> extends ArrayList<E> {
     public E pop() {
         if (delegate.isEmpty()) throw new EmptyStackException();
         E e = delegate.get(delegate.size() -1);
-        remove(delegate.size() -1);
+        delegate.remove(delegate.size() -1);
         return e;
     }
 }
